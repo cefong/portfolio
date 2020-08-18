0d788485a8887ea6b5afb462971042f0ebcae276
cefong <celinefong4@gmail.com>
2020-08-18 16:34:09 -0600

    Updates

---
+++ precache-manifest.23580a6440684e065f3e279d8afe5911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b10b6207eef17038f9fd373903caac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "52cd8cc95a714aafbffb",
+    "url": "/portfolio/static/css/main.3180bdec.chunk.css"
+  },
+  {
+    "revision": "c6359446bc64555e6408",
+    "url": "/portfolio/static/js/2.9297f1d3.chunk.js"
+  },
+  {
+    "revision": "52cd8cc95a714aafbffb",
+    "url": "/portfolio/static/js/main.9a4e5824.chunk.js"
+  },
+  {
+    "revision": "3593b2f94172585f4863",
+    "url": "/portfolio/static/js/runtime-main.3b58c541.js"
+  }
+]);
